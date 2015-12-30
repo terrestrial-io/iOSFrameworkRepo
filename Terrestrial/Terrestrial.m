fa13deaa2a8e06177f3820a9e7fcbc4ed1b074f0
Joseph Mambwe <josephmambwe@me.com>
2015-12-30 20:23:31 +0000

    Fixed GUI checkbox bug

--- Terrestrial/Terrestrial.m
+++ Terrestrial/Terrestrial.m
@@ -22,15 +22,12 @@
     return sharedInstance;
 }
 
-
-
-- (NSString *) stringForKey: (NSString *) stringToTranslate andContext: (NSString *) contextString {
-    
+- (void) loadTranslationFile {
     
     
     NSString* localeToShow;
     NSString *bundlePath = [[NSBundle mainBundle] resourcePath];
-
+    
     
     if (!manualLocale ) {
         
@@ -41,33 +38,31 @@
         
     }
     
-    //NSLog(@"Locale to show: %@",localeToShow);
-    
     if (!retrievedStrings) {
         
         retrievedStrings = [[NSArray alloc] init];
-
+        
         
         NSString *translationsFolder = [bundlePath stringByAppendingPathComponent:@"Translations"];
         
         NSString *localeFilePath = [translationsFolder stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.json",localeToShow]];
         
-
-       // NSLog(@"File to get: %@",localeFilePath);
+        
+        // NSLog(@"File to get: %@",localeFilePath);
         
         
         
         if ([[NSFileManager defaultManager] fileExistsAtPath:localeFilePath]) {
-    
-    NSString *jsonString = [[NSString alloc] initWithContentsOfFile:localeFilePath encoding:NSUTF8StringEncoding error:NULL];
-    
-    NSError *jsonError ;
-    
-    
-    retrievedStrings = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:&jsonError];
             
-       
-        
+            NSString *jsonString = [[NSString alloc] initWithContentsOfFile:localeFilePath encoding:NSUTF8StringEncoding error:NULL];
+            
+            NSError *jsonError ;
+            
+            
+            retrievedStrings = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:&jsonError];
+            
+            
+            
         } else {
             
             
@@ -86,7 +81,7 @@
                 
                 
                 retrievedStrings = [NSJSONSerialization JSONObjectWithData:[jsonString dataUsingEncoding:NSUTF8StringEncoding] options:NSJSONReadingMutableContainers error:&jsonError];
-
+                
                 
             }
             
@@ -97,19 +92,33 @@
         }
         
         
-   
+        
     }
     
-    ////NSlog(@"RETRIEVED STRINGS %@",retrievedStrings);
+
     
+    
+}
+
+
+- (NSArray *) filterTranslationsArrayWithPredicate:(NSPredicate *) predicate {
+    
+    return [retrievedStrings filteredArrayUsingPredicate:predicate];
+    
+}
+
+
+- (NSString *) stringForKey: (NSString *) stringToTranslate andContext: (NSString *) contextString {
+    
+    [self loadTranslationFile];
+    
+
     NSString *type = @"default";
     
     
     NSPredicate *bPredicate = [NSPredicate predicateWithFormat:@"(SELF.string ==[cd] %@) AND (SELF.context ==[cd] %@)",stringToTranslate ,contextString,type];
     
    NSArray* filteredArray = [retrievedStrings filteredArrayUsingPredicate:bPredicate];
-
-   //NSlog(@"HERE %@",filteredArray);
 
     
     NSString *translatedString;
@@ -133,7 +142,7 @@
     return stringToTranslate;
     }
     
-    //[NSString stringWithFormat:[@"Hello: %@ , how are you?" translatedWithContext:@""], translatedString];
+
     
 }
 
@@ -971,6 +980,188 @@ NSArray * trstlLocalizedPluralStringKeyForCountAndSingularNounForLanguage(NSUInt
     
     
 }
+
+- (NSString *) retrievePluralRuleForCount: (NSUInteger) count andLanguageCode: (NSString *)languageCode {
+    
+    NSString *pluralRule = nil;
+    
+    // Because -hasPrefix is being used here, any three-letter ISO 639-2/3 codes must come before two-letter ISO 639-1 codes in order to prevent, for instance, Konkani (kok) from having Korean (ko) pluralization applied
+    
+    if ([languageCode hasPrefix:@"ar"]) {
+        pluralRule = trstlArabicPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"ca"]) {
+        pluralRule = trstlCatalanPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"zh-Hans"]) {
+        pluralRule = trstlSimplifiedChinesePluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"zh-Hant"]) {
+        pluralRule = trstlTraditionalChinesePluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"cr"]) {
+        pluralRule = trstlCroatianPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"cs"]) {
+        pluralRule = trstlCzechPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"da"]) {
+        pluralRule = trstlDanishPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"nl"]) {
+        pluralRule = trstlDutchPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"en"]) {
+        pluralRule = trstlEnglishPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"fr"]) {
+        pluralRule = trstlFrenchPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"de"]) {
+        pluralRule = trstlGermanPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"fi"]) {
+        pluralRule = trstlFinnishPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"el"]) {
+        pluralRule = trstlGreekPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"he"]) {
+        pluralRule = trstlHebrewPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"hu"]) {
+        pluralRule = trstlHungarianPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"id"]) {
+        pluralRule = trstlIndonesianPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"it"]) {
+        pluralRule = trstlItalianPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"ja"]) {
+        pluralRule = trstlJapanesePluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"ko"]) {
+        pluralRule = trstlKoreanPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"lv"]) {
+        pluralRule = trstlLatvianPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"ms"]) {
+        pluralRule = trstlMalayPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"nb"]) {
+        pluralRule = trstlNorwegianBokmalPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"nn"]) {
+        pluralRule = trstlNorwegianNynorskPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"pl"]) {
+        pluralRule = trstlPolishPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"pt"]) {
+        pluralRule = trstlPortuguesePluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"ro"]) {
+        pluralRule = trstlRomanianPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"ru"]) {
+        pluralRule = trstlRussianPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"es"]) {
+        pluralRule = trstlSpanishPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"sk"]) {
+        pluralRule = trstlSlovakPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"sv"]) {
+        pluralRule = trstlSwedishPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"th"]) {
+        pluralRule = trstlThaiPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"tr"]) {
+        pluralRule = trstlTurkishPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"uk"]) {
+        pluralRule = trstlUkrainianPluralRuleForCount(count);
+    } else if ([languageCode hasPrefix:@"vi"]) {
+        pluralRule = trstlVietnamesePluralRuleForCount(count);
+    } else {
+        
+        NSLog(@"Unsupported language for pluralisation : %@", languageCode);
+        return nil;
+        
+    }
+    
+    return pluralRule;
+    
+}
+
+
++ (NSString *) pluralStringWithCount: (int)count
+                                  id:(NSString *)idString
+                                 one:(NSString *)oneString
+                               other:(NSString *)otherString
+                             context: (NSString *) contextString {
+    
+   
+    
+    NSString *translatedString = [[Terrestrial sharedInstance] pluralStringLookupWithId: idString andCount:count];
+    
+    
+    
+    if (translatedString != nil) {
+        
+        return translatedString;
+        
+    } else {
+        
+        
+        if (count == 1) {
+            
+            return  [NSString stringWithFormat:oneString,count];
+        
+        } else {
+    
+            return [NSString stringWithFormat:otherString,count];;
+            
+        }
+        
+        
+    }
+   
+    
+}
+
+
+- (NSString *) pluralStringLookupWithId:(NSString *)idString andCount:(int) count {
+    
+    [self loadTranslationFile];
+    
+    // Retrieve pluralisation rule from count & language code
+    NSString *languageCode = [[[NSBundle mainBundle] preferredLocalizations] objectAtIndex:0];
+    
+    NSLog(@"BOOM 1");
+    
+    NSString *retrievedRule = [[Terrestrial sharedInstance] retrievePluralRuleForCount:count andLanguageCode:languageCode];
+     NSLog(@"BOOM 2");
+    
+    
+    if (retrievedRule) {
+
+         NSLog(@"BOOM 3");
+    // Perform lookup using plural id
+    
+    NSString *type = @"plural_string";
+    
+    NSPredicate *bPredicate = [NSPredicate predicateWithFormat:@"(SELF.plural_string.id ==[cd] %@) AND (SELF.type ==[cd] %@)",idString ,type];
+    
+        
+    NSArray* filteredArray = [[Terrestrial sharedInstance] filterTranslationsArrayWithPredicate:bPredicate];
+    
+     NSLog(@"BOOM 4");
+        
+    NSString *translatedString;
+    NSDictionary *translatedObject;
+    
+    
+    if ([filteredArray count] > 0 ) {
+        
+        
+        NSDictionary * stringDict = [filteredArray objectAtIndex:0];
+        
+        translatedObject = [stringDict objectForKey:@"translation"];
+        
+        translatedString = [translatedObject objectForKey:retrievedRule];
+    }
+    
+    
+   
+    NSLog(@"BOOM 5");
+        if(translatedString) { return [NSString stringWithFormat:translatedString,count];};
+        
+        NSLog(@"BOOM 6");
+
+        return nil;
+        
+    } else {
+        
+        return nil;
+        
+    }
+    
+    
+}
+
 
 
 
