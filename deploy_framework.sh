afd13fee838748a095cd00374bcc6c4aecef32fe
Joseph Mambwe <josephmambwe@me.com>
2016-01-21 23:15:45 +0000

    Added deployment script

--- deploy_framework.sh
+++ deploy_framework.sh
@@ -1,13 +1,20 @@
 
+xcodebuild -project Terrestrial.xcodeproj -scheme Terrestrial -sdk iphonesimulator -configuration 'Release' -destination 'platform=iOS Simulator,name=iPhone 6S,OS=latest' clean build
+
+cd iOSFrameworkRepo
+
 echo Current Framework Version is "$(git tag)" 
 echo Enter new version number
 read input_variable
 echo "You entered: $input_variable" 
 
-cd iOSFrameworkRepo
-
 git add -A
 git commit -m "Build version $input_variable"
 git tag $input_variable
 git push --all
+git push --tags 
+
+
+
+
 
