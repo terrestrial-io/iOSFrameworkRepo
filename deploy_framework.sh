073a04dcf52787ef4b7670b6436029610c7c7155
Joseph Mambwe <josephmambwe@me.com>
2016-01-21 22:58:42 +0000

    Build version 0.1.1

---
+++ deploy_framework.sh
@@ -0,0 +1,12 @@
+
+echo Current Framework Version is "$(git tag)" 
+echo Enter new version number
+read input_variable
+echo "You entered: $input_variable" 
+
+cd /iOSFrameworkRepo
+git add -A
+git commit -m "Build version $input_variable"
+git tag $input_variable
+git checkout -b master
+git push origin master
