68fb2777c15bd446d44e0590f08f8f3b004c1cb3
Joseph Mambwe <josephmambwe@me.com>
2016-01-24 23:29:42 +0000

    About to deploy

--- deploy_framework.sh
+++ deploy_framework.sh
@@ -8,12 +8,19 @@ echo Enter new version number
 read input_variable
 echo "You entered: $input_variable" 
 
+sed -i '' 's/\(s.version.*=.*\"\)\(.*\)\"/s.version          = \"$input_variable\"/1' Terrestrial.podspec
+
 git add -A
 git commit -m "Build version $input_variable"
 git tag $input_variable
 git push --all
 git push --tags 
 
+cd ..
+
+git add -A
+git commit -m "Released version $input_variable"
+git push --all
 
 
 
