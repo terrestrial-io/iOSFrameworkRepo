1337fda009f05c89be99c5500e027783dbdbcfd1
Joseph Mambwe <josephmambwe@me.com>
2016-02-26 13:51:59 +0000

    Released version 0.3.6-beta4

--- Terrestrial/TerrestrialUICategoryAdditions.h
+++ Terrestrial/TerrestrialUICategoryAdditions.h
@@ -11,38 +11,38 @@ IB_DESIGNABLE
 
 @interface UILabel (TerrestrialLabel)
 @property (nonatomic) IBInspectable BOOL Terrestrial;
-@property (nonatomic) IBInspectable NSString *identifier;
+@property (nonatomic) IBInspectable NSString *Identifier;
 
 @end
 
 @interface UIButton (TerrestrialButton)
 @property (nonatomic) IBInspectable BOOL Terrestrial;
-@property (nonatomic) IBInspectable NSString *identifier;
+@property (nonatomic) IBInspectable NSString *Identifier;
 @end
 
 @interface UITextField (TerrestrialTextField)
 @property (nonatomic) IBInspectable BOOL Terrestrial;
-@property (nonatomic) IBInspectable NSString *identifier;
+@property (nonatomic) IBInspectable NSString *Identifier;
 @end
 
 @interface UITextView (TerrestrialTextView)
 
 @property (nonatomic) IBInspectable BOOL Terrestrial;
-@property (nonatomic) IBInspectable NSString *identifier;
+@property (nonatomic) IBInspectable NSString *Identifier;
 
 @end
 
 @interface UIBarButtonItem (TerrestrialBarButtonItem)
 @property (nonatomic) IBInspectable BOOL Terrestrial;
-@property (nonatomic) IBInspectable NSString *identifier;
+@property (nonatomic) IBInspectable NSString *Identifier;
 @end
 
 @interface UINavigationItem (TerrestrialNavigationItem)
 @property (nonatomic) IBInspectable BOOL Terrestrial;
-@property (nonatomic) IBInspectable NSString *identifier;
+@property (nonatomic) IBInspectable NSString *Identifier;
 @end
 
 @interface UIViewController (TerrestrialViewController)
 @property (nonatomic) IBInspectable BOOL Terrestrial;
-@property (nonatomic) IBInspectable NSString *identifier;
+@property (nonatomic) IBInspectable NSString *Identifier;
 @end
